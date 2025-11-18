1f31978209352f58a4a1ed43a2af3ad57d69cf0e
Ebin234 <ebinyesudas234@gmail.com>
2025-11-18 23:48:03 +0530

    Finished building hackPost creation route

--- src/controllers/hackPostController.js
+++ src/controllers/hackPostController.js
@@ -1,6 +1,17 @@
-const createHackPost = (req, res) => {
+const HackPostModel = require("../models/hackPostModel");
+
+const createHackPost = async (req, res) => {
   try {
-    res.json({ data: req.user });
+    const { hackName, description, imageUrl } = req.body;
+    const hackPost = new HackPostModel({
+      hackName,
+      description,
+      imageUrl,
+      organizer: req.user._id,
+    });
+    console.log({ hackPost });
+    await hackPost.save();
+    res.status(201).json({ message: `${hackName} created successfully` });
   } catch (err) {
     res.status(400).json({ message: err.message });
   }
--- src/models/hackPostModel.js
+++ src/models/hackPostModel.js
@@ -11,8 +11,12 @@ const HackPostSchema = new mongoose.Schema({
   },
   imageUrl: {
     type: String,
+    default:"https://imgs.search.brave.com/FhMcYnc9KzAXz3c9GTKAAnAyVjx7dE8VOmhHOwnNp7g/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG5p/Lmljb25zY291dC5j/b20vaWxsdXN0cmF0/aW9uL3ByZW1pdW0v/dGh1bWIvZW1wdHkt/ZmlsZS1pbGx1c3Ry/YXRpb24tc3ZnLWRv/d25sb2FkLXBuZy0x/MTgzODI1MS5wbmc"
   },
-  users: {
+  requestedUsers: {
+    type: [mongoose.Schema.Types.ObjectId],
+  },
+  acceptedUsers: {
     type: [mongoose.Schema.Types.ObjectId],
   },
   organizer: {
