6d24c899c8b2b075bdfba2c7519015837f22fb57
Ebin234 <ebinyesudas234@gmail.com>
2025-11-20 01:48:21 +0530

    Created joinRequestModel and updated the /user/send-request/:id route

--- src/controllers/userRoutesController.js
+++ src/controllers/userRoutesController.js
@@ -1,4 +1,5 @@
 const HackPostModel = require("../models/hackPostModel");
+const JoinRequestModel = require("../models/joinRequestModel");
 
 const getAllPosts = async (req, res) => {
   try {
@@ -18,22 +19,23 @@ const createJoinRequest = async (req, res) => {
     const user = req.user;
     let post = await HackPostModel.findById(id);
     if (!post) {
-      throw new Error("Post id not found in DB");
+      throw new Error("Post not found");
     }
-    const userExist = post.requestedUsers.includes(user._id);
+    const userExist = await JoinRequestModel.findOne({
+      user: user._id,
+      hackPostId: post._id,
+    });
     if (userExist) {
       throw new Error("User already Exist");
     } else {
-      const data = await HackPostModel.findByIdAndUpdate(
-        id,
-        {
-          $push: {
-            requestedUsers: user._id,
-          },
-        },
-        { returnDocument: "after" }
-      );
-      res.json({ message: "request sent successfully" });
+      const joinRequest = new JoinRequestModel({
+        user: user._id,
+        organizer: post.organizer,
+        hackPostId: post._id,
+      });
+      console.log({ joinRequest });
+      await joinRequest.save();
+      res.status(201).json({ message: "request created successfully" });
     }
   } catch (err) {
     res.status(400).json({ message: err.message });
---
+++ src/models/joinRequestModel.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const JoinRequestSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  organizer: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+  },
+  hackPostId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "HackPost",
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: {
+      values: ["PENDING", "ACCEPTED", "REJECTED"],
+      message: `{VALUE} is not supported`,
+    },
+    default:"PENDING"
+  },
+});
+
+module.exports = mongoose.model("JoinRequest", JoinRequestSchema);
