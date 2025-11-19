1ed61a598c1588e1bb468735c3ad7a98426587f1
Ebin234 <ebinyesudas234@gmail.com>
2025-11-19 21:54:22 +0530

    /user/send-request/:id route created

--- src/controllers/userRoutesController.js
+++ src/controllers/userRoutesController.js
@@ -8,16 +8,41 @@ const getAllPosts = async (req, res) => {
     console.log({ posts });
     res.json({ data: posts, message: "fetch success" });
   } catch (err) {
-
     res.status(400).json({ message: err.message });
   }
 };
 
-
+const createJoinRequest = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = req.user;
+    let post = await HackPostModel.findById(id);
+    if (!post) {
+      throw new Error("Post id not found in DB");
+    }
+    const userExist = post.requestedUsers.includes(user._id);
+    if (userExist) {
+      throw new Error("User already Exist");
+    } else {
+      const data = await HackPostModel.findByIdAndUpdate(
+        id,
+        {
+          $push: {
+            requestedUsers: user._id,
+          },
+        },
+        { returnDocument: "after" }
+      );
+      res.json({ message: "request sent successfully" });
+    }
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
 
 // try {
 //   } catch (err) {
 //     res.status(400).json({ message: err.message });
 //   }
 
-module.exports = { getAllPosts };
+module.exports = { getAllPosts, createJoinRequest };
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,7 +1,11 @@
 const router = require("express").Router();
-const { getAllPosts } = require("../controllers/userRoutesController");
+const {
+  getAllPosts,
+  createJoinRequest,
+} = require("../controllers/userRoutesController");
 const commonAuth = require("../middlewares/commonAuth");
 
 router.get("/user/get-all-hack-posts", commonAuth, getAllPosts);
+router.post("/user/send-request/:id", commonAuth, createJoinRequest);
 
 module.exports = router;
